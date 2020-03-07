0755d4771d669ef0f974241d8bd8d8cc1c00d46e
Ismael Santana <ismaelfranciscosb@gmail.com>
2020-03-07 00:27:42 -0400

    Ya tiene el bottom sheet

--- app/src/main/java/com/example/smartlist/ListaDeCompra.java
+++ app/src/main/java/com/example/smartlist/ListaDeCompra.java
@@ -12,6 +12,8 @@ import android.widget.Adapter;
 import android.widget.CheckBox;
 import android.widget.ListView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 
 
@@ -22,6 +24,7 @@ public class ListaDeCompra extends Fragment {
 
     private View rootView;
     private ArrayList<Lista1> listaItem;
+    private FloatingActionButton floatingActionButton;
     private Adatador adaptador;
     private ListView listView;
     private CheckBox chkAll;
@@ -39,6 +42,18 @@ public class ListaDeCompra extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_lista_de_compra, container, true);
 
         listView = rootView.findViewById(R.id.listView1);
+        floatingActionButton = rootView.findViewById(R.id.floatingActionButton);
+
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UtilizandoButtonSheet bottonSheet = new UtilizandoButtonSheet();
+
+
+                bottonSheet.show(getFragmentManager(), "exampleBottonSheet");
+            }
+        });
 
         adaptador = new Adatador(getContext(), getArrayList(), cantidadTotal);
 
@@ -52,11 +67,11 @@ public class ListaDeCompra extends Fragment {
         listaItem = new ArrayList<Lista1>();
 
         listaItem.add(new Lista1("Pan de agua", 10, false));
-        listaItem.add(new Lista1("Pan de agua", 10, false));
-        listaItem.add(new Lista1("Pan de agua", 10, false));
-        listaItem.add(new Lista1("Pan de agua", 10, false));
-        listaItem.add(new Lista1("Pan de agua", 10, false));
-        listaItem.add(new Lista1("Pan de agua", 10, false));
+        listaItem.add(new Lista1("Arina de maiz", 10, false));
+        listaItem.add(new Lista1("Arroz primiun", 1, false));
+        listaItem.add(new Lista1("Refresco de lo grande", 4, false));
+        listaItem.add(new Lista1("Pan de agua", 6, false));
+        listaItem.add(new Lista1("Pan de agua", 9, false));
         listaItem.add(new Lista1("Pan de agua", 10, false));
         listaItem.add(new Lista1("Pan de agua", 10, false));
         listaItem.add(new Lista1("Pan de agua", 10, false));
